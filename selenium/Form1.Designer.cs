258a54fe1d12ac7e6f41db388200653272028ed4
workmail20 <74146677+workmail20@users.noreply.github.com>
2021-03-19 16:55:01 +0200

    Add files via upload

---
+++ selenium/Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿
+namespace selenium
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(159, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(234, 29);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Start Chrome";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(159, 47);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(234, 29);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Stop Chrome";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(159, 82);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(234, 29);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Yahoo work";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(13, 117);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(234, 29);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Youtube work 1";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(13, 152);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(517, 118);
+            this.textBox1.TabIndex = 4;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(296, 117);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(234, 29);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "Youtube work 2";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(13, 283);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(233, 24);
+            this.button6.TabIndex = 6;
+            this.button6.Text = "Execute JS";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(13, 313);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(233, 64);
+            this.textBox2.TabIndex = 7;
+            this.textBox2.Text = "alert(\'test JS\');";
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(296, 283);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(234, 24);
+            this.button7.TabIndex = 8;
+            this.button7.Text = "Tabs work";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(296, 320);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(234, 25);
+            this.button8.TabIndex = 9;
+            this.button8.Text = "Wait test";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(541, 387);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Selenium work";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+    }
+}
+
---
+++ selenium/Form1.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium.Chrome;
+
+namespace selenium
+{
+    public partial class Form1 : Form
+    {
+        IWebDriver driver;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+
+            /*
+            driver.Navigate().GoToUrl("http://google.com");
+
+            IWebElement SearchInput = driver.FindElement(By.Name("q"));
+            SearchInput.SendKeys("последние фильмы" + OpenQA.Selenium.Keys.Enter);
+            */
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            driver.Quit();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            driver.Navigate().GoToUrl("https://yahoo.com");
+
+            IWebElement element = driver.FindElement(By.PartialLinkText("Finance"));
+            element.Click();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            driver.Navigate().GoToUrl("https://youtube.com");
+            List<IWebElement> News = driver.FindElements(By.CssSelector("#contents h3 a")).ToList();
+
+            for (int i = 0; i < News.Count; i++)
+            {
+                textBox1.AppendText(News[i].GetAttribute("aria-label")+ "\r\n------------------------\r\n");
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            driver.Navigate().GoToUrl("https://youtube.com");
+            List<IWebElement> News = driver.FindElements(By.CssSelector("#contents h3 a")).ToList();
+
+            String s;
+            for (int i = 0; i < News.Count; i++)
+            {
+                s = News[i].GetAttribute("aria-label");
+
+                if (s.StartsWith("М"))
+                {
+                    textBox1.AppendText("Видео № " +(i+1).ToString()+ " начинается с текста 'М'" + "\r\n------------------------\r\n");
+                }
+                if (s.EndsWith("!"))
+                {
+                    textBox1.AppendText("Видео № " + (i + 1).ToString() + " заканчивается текстом '!'" + "\r\n------------------------\r\n");
+
+                }
+                if (s.Contains("серия"))
+                {
+                    textBox1.AppendText("Видео № " + (i + 1).ToString() + " содержит текст 'серия'" + "\r\n------------------------\r\n");
+                    News[i].Click();
+                    break;
+                }
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            IJavaScriptExecutor jse = driver as IJavaScriptExecutor;
+            jse.ExecuteScript(textBox2.Text);
+        }
+
+        private string FindWindow(String Url)
+        {
+            String StartWindow = driver.CurrentWindowHandle;
+            String Result = "";
+            for(int i=0; i < driver.WindowHandles.Count; i++)
+            {
+                if (driver.WindowHandles[i] != StartWindow)
+                {
+                    driver.SwitchTo().Window(driver.WindowHandles[i]);
+                    if (driver.Url.Contains(Url))
+                    {
+                        Result = driver.WindowHandles[i];
+                        break;
+                    }
+                }
+            }
+            driver.SwitchTo().Window(StartWindow);
+            return Result;
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            IJavaScriptExecutor jse = driver as IJavaScriptExecutor;
+
+            driver.Navigate().GoToUrl("https://youtube.com");
+            jse.ExecuteScript("window.open()");
+            driver.SwitchTo().Window(driver.WindowHandles[1]);
+
+            driver.Navigate().GoToUrl("https://google.com");
+            jse.ExecuteScript("window.open()");
+            driver.SwitchTo().Window(driver.WindowHandles[2]);
+
+            driver.Navigate().GoToUrl("https://yahoo.com");
+
+            driver.SwitchTo().Window(driver.WindowHandles[0]);
+            System.Windows.Forms.MessageBox.Show(driver.Title + "\r\n" + driver.Url);
+
+            driver.SwitchTo().Window(driver.WindowHandles[1]);
+            System.Windows.Forms.MessageBox.Show(driver.Title + "\r\n" + driver.Url);
+
+            driver.SwitchTo().Window(driver.WindowHandles[2]);
+            System.Windows.Forms.MessageBox.Show(driver.Title + "\r\n" + driver.Url);
+
+            String TabWindow = FindWindow("youtube");
+            driver.SwitchTo().Window(TabWindow);
+            System.Windows.Forms.MessageBox.Show(driver.Title + "\r\n" + driver.Url);
+
+
+
+
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            driver.Navigate().GoToUrl("https://youtube.com");
+
+            IWebElement input = driver.FindElement(By.CssSelector("#search"));
+
+            input.SendKeys("ТЕХНОЛОГИИ КОТОРЫЕ ИЗМЕНЯТ МИР" + OpenQA.Selenium.Keys.Enter);
+            WebDriverWait ww = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+            IWebElement txt = ww.Until(ExpectedConditions.ElementExists(By.CssSelector("#description-text")));
+            textBox1.Text=(txt.Text);
+        }
+    }
+}
